5d633a21434c5ee0f3aa5730956f0693db4bb98c
Hemant soni <sonihemant4741@gmail.com>
2023-01-16 21:38:28 +0530

    Update hero banner component

--- frontend/component/HeroBanner.jsx
+++ frontend/component/HeroBanner.jsx
@@ -1,26 +1,29 @@
-import Link from 'next/link'
-import React from 'react'
+import React from 'react';
+import Link from 'next/link';
 
-function HeroBanner() {
+import { urlFor } from '../lib/client';
+
+const HeroBanner = ({ heroBanner }) => {
   return (
-    <div className="her-banner-container">
+    <div className="hero-banner-container">
       <div>
-        <p className="beats-solo"></p>
-        <h3></h3>
-        <img src="" alt="headphone" className="hero-banner-image" srcset="" />
+        <p className="beats-solo">{heroBanner.smallText}</p>
+        <h3>{heroBanner.midText}</h3>
+        <h1>{heroBanner.largeText1}</h1>
+        <img src={urlFor(heroBanner.image)} alt="headphones" className="hero-banner-image" />
+
         <div>
-          <Link href="/product/ID">
-            <button type="butto"></button>
+          <Link href={`/product/${heroBanner.product}`}>
+            <button type="button">{heroBanner.buttonText}</button>
           </Link>
           <div className="desc">
             <h5>Description</h5>
-            <p></p>
+            <p>{heroBanner.desc}</p>
           </div>
         </div>
       </div>
-
     </div>
   )
 }
 
-export default HeroBanner
+export default HeroBanner;
--- frontend/lib/client.js
+++ frontend/lib/client.js
@@ -1,10 +1,14 @@
 import sanityClient from '@sanity/client';
 import imageUrlBuilder from '@sanity/image-url';
 
-const client = sanityClient({
+export const client = sanityClient({
   projectId: 'oaxsf68n',
   dataset: 'production',
   apiVersion: '2023-01-16',
   useCdn: true,
   token: process.env.SANITY_TOKEN
 })
+
+const builder = imageUrlBuilder(client);
+
+export const urlFor = (source) => builder.image(source);
