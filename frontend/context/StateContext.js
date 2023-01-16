8cb1b358ec05b62729f243ffb702a59703231c20
Hemant soni <sonihemant4741@gmail.com>
2023-01-17 01:01:33 +0530

    Add function to add item to cart

--- frontend/context/StateContext.js
+++ frontend/context/StateContext.js
@@ -13,6 +13,43 @@ export const StateContext = ( { children }) => {
   const [totalQuantities, setTotalQuantities] = useState();
   const [qty, setQty] = useState(1);
 
+ const onAdd = (product, quantity) => {
+   const checkProductInCart = cartItems.find((item) => item._id === product._id);
+   setTotalPrice((prevTotalPrice) => prevTotalPrice + product.price * quantity);
+   setTotalQuantities((prevTotalQuantities) => prevTotalQuantities + quantity);
+
+   if(checkProductInCart) {
+     setTotalPrice((prevTotalPrice) => prevTotalPrice + product.price * quantity);
+     setTotalQuantities((prevTotalQuantities) => prevTotalQuantities + quantity);
+
+     const updateCartItems = cartItems.map((cartProduct) => {
+       if(cartProduct._id === product._id) return {
+         ...cartProduct,
+         quantity: cartProduct.quantity + quantity
+       } 
+     })
+
+     setCartitems(updateCartItems);
+     
+   } else {
+    product.quantity = quantity;
+    setCartitems([...cartItems, { ...product }]);
+   }
+   toast.success(`${qty} ${product.name} added tot he cart.`)
+ }
+
+  const incQty = () => {
+    setQty((prevQty) => prevQty + 1 );
+  }
+
+
+  const decQty = () => {
+    setQty((prevQty) => {
+      if(prevQty-1 < 1) return 1;
+     return prevQty- 1 
+    });
+  }
+
   return (
     <Context.Provider
      value={{
@@ -20,10 +57,15 @@ export const StateContext = ( { children }) => {
        cartItems,
        totalPrice,
        totalQuantities,
-       qty
+       qty,
+       incQty,
+       decQty, 
+       onAdd
      }}
     >
       {children}
     </Context.Provider>
   )
 }
+
+export const useStateContext = () => useContext(Context);
