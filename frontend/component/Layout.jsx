095181102de11eecf77521cfd05d62c32ffbf6ae
Hemant soni <sonihemant4741@gmail.com>
2023-01-16 23:11:44 +0530

    Layout component update

--- frontend/component/Layout.jsx
+++ frontend/component/Layout.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React from 'react';
+import Head from 'next/head';
 
-function Layout() {
+import Navbar from './Navbar';
+import Footer from './Footer';
+
+const Layout = ({ children }) => {
   return (
-    <div>Layout</div>
+    <div className="layout">
+      <Head>
+        <title>Buy Now</title>
+      </Head>
+      <header>
+        <Navbar />
+      </header>
+      <main className="main-container">
+        {children}
+      </main>
+      <footer>
+        <Footer />
+      </footer>
+    </div>
   )
 }
 
-export default Layout
+export default Layout;
--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,6 +1,11 @@
-import '../styles/globals.css'
-
+import React from 'react';
+import '../styles/globals.css';
+import { Layout } from '../component';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  );
 }
