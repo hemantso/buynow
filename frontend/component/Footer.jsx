1183a592ccb348e4cb758d9124524e569ac24d5a
Hemant soni <sonihemant4741@gmail.com>
2023-01-16 23:54:36 +0530

    Update product details component

--- frontend/component/Footer.jsx
+++ frontend/component/Footer.jsx
@@ -1,8 +1,15 @@
-import React from 'react'
+import React from 'react';
+import { AiFillInstagram, AiOutlineTwitter} from 'react-icons/ai';
 
-function Footer() {
+const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className="footer-container">
+      <p>2023 Buy Now Headphones All rights reserverd</p>
+      <p className="icons">
+        <AiFillInstagram />
+        <AiOutlineTwitter />
+      </p>
+    </div>
   )
 }
 
