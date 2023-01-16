781d38a557ebb0adf65103bd5f53c1e8815b232b
Hemant soni <sonihemant4741@gmail.com>
2023-01-17 00:32:19 +0530

    Implement useContext hook

---
+++ frontend/context/StateContext.js
@@ -0,0 +1,29 @@
+import React, { createContext, useContext, useState, useEffect } from 'react';
+
+import { toast } from 'react-hot-toast';
+
+
+const Context = createContext();
+
+export const StateContext = ( { children }) => {
+  const [showCart, setShowCart] = useState(false);
+  const [cartItems, setCartitems] = useState();
+
+  const [totalPrice, setTotalPrice] = useState();
+  const [totalQuantities, setTotalQuantities] = useState();
+  const [qty, setQty] = useState(1);
+
+  return (
+    <Context.Provider
+     value={{
+       showCart,
+       cartItems,
+       totalPrice,
+       totalQuantities,
+       qty
+     }}
+    >
+      {children}
+    </Context.Provider>
+  )
+}
